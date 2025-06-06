60e6dce9336d037478b83a447253bf9ade2419aa
HoannnLee <Leecghoan@gmail.com>
2025-06-06 12:06:29 +0700

    login page, api login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,77 @@
-const Login = () => {
-    return (  
-        <div>Login Success</div>
+
+import { Button, Form, Input, notification } from 'antd';
+import { loginAPI } from '../utils/api';
+import { useNavigate } from 'react-router-dom';
+
+
+
+function Login() {
+
+    const navigate = useNavigate();
+    const onFinish = async (values) => {
+
+        const {email, password} = values;
+
+        const res = await loginAPI(email, password);
+
+        if(res && res.EC === 0){
+            localStorage.setItem('access_token', res.access_token); 
+            notification.success({
+                message: 'Đăng nhập thành công',
+                description: 'Bạn đã đăng nhập tài khoản thành công',
+            })
+            navigate('/')
+        }
+        else {
+            notification.error({
+                message: 'Đăng nhập thất bại',
+                description: res?.EM ?? 'Đã xảy ra lỗi trong quá trình đăng nhập. Vui lòng thử lại sau.',
+            });
+
+        }
+
+        console.log('Success:', values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    return (
+
+        <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600 ,marginTop: '50px', marginLeft: '20px' }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+            layout='vertical'
+        >
+            <Form.Item
+                label="Email"
+                name="email"
+                rules={[{ required: true, message: 'Vui lòng nhập email của bạn!' }]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="Mật khẩu"
+                name="password"
+                rules={[{ required: true, message: 'Vui lòng nhập mật khẩu!' }]}
+            >
+                <Input.Password />
+            </Form.Item>
+
+            <Form.Item >
+                <Button type="primary" htmlType="submit">
+                    Đăng nhập
+                </Button>
+            </Form.Item>
+        </Form>
     );
 }
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -12,6 +12,17 @@ const createUserAPI = (email , name , password) =>{
     return axios.post(URL_API , data)
 }
 
+
+const loginAPI = (email , password) =>{
+    const URL_API ="/v1/api/login" ;
+    const data = {
+        email,
+        password
+    }
+
+    return axios.post(URL_API , data)
+}
+
 export {
-    createUserAPI,
+    createUserAPI,loginAPI
 }
